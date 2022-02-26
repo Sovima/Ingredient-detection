7e1208e9d85356e5a649b9f19c533ebeee728310
Sovima <malashchenko1329@gmail.com>
2022-02-26 15:21:08 -0500

    Get script in

--- collectData.py
+++ collectData.py
@@ -1,2 +1,20 @@
 import openfoodfacts
 import numpy
+
+def main(ingredients):
+    for ingridient in ingredients:
+    # print(ingridient['name'])
+        search_result = openfoodfacts.products.advanced_search({
+            "search_terms": ingridient,
+            "sort_by":"unique_scans_n",
+            "json" : 1
+        })
+        if(len(search_result['products'])==0):
+            print("search for ingredient " + ingridient + " returned empty")
+            continue
+        if('image_url' not in search_result['products'][0]):
+            print("ingredient " + ingridient + " has no image")
+            continue
+        ingridient_image = search_result['products'][0]['image_url']
+        print(ingridient_image)
+main(["tomato", "potato", "apple", "pasta", "tuna", "ketchup"])
