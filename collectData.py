a31050dac00a2003056efae9e8441298e34b98a1
Sovima <malashchenko1329@gmail.com>
2022-02-26 20:49:16 -0500

    Get ingredients

--- collectData.py
+++ collectData.py
@@ -3,26 +3,31 @@ import numpy
 
 def main(ingredients):
     for ingridient in ingredients:
-    # print(ingridient['name'])
         search_result = openfoodfacts.products.advanced_search({
             "search_terms": ingridient,
             "sort_by":"unique_scans_n",
             "json" : 1
         })
-        if(len(search_result['products'])==0):
+        if (len(search_result['products']) == 0):
             print("search for ingredient " + ingridient + " returned empty")
             continue
-        if('image_url' not in search_result['products'][0]):
-            print("ingredient " + ingridient + " has no image")
-            continue
+        if ('image_url' not in search_result['products'][0]):
+        	i = 0
+        	while 'image_url' not in search_result['products'][i]:
+        		i += 1
+        	ingridient_image = search_result['products'][i]['image_url']
+        	print(ingridient_image)
+        	continue
+
         ingridient_image = search_result['products'][0]['image_url']
         print(ingridient_image)
-main(['salt', 'water', 'garlic clove', 
-                       'salt and pepper', 'all-purpose flmy',
+main(["brown sugar"])
+'''main(['salt', 'water', 'garlic clove', 
+                       'salt and pepper', 'all-purpose flour',
                        'baking soda', 'vanilla', 'butter',
                        'egg', 'onion', 'sugar', 'olive oil', 
-                       'milk', 'flmy', 'pepper', 'brown sugar', 
+                       'milk', 'flour', 'pepper', 'brown sugar', 
                        'garlic', 'baking powder','scallion',
                        'cheddar','parmesan cheese', 'lemon juice',
                        'vegetable oil', 'carrot', 'sour cream', 
-                       'cinnamon', 'black pepper'])
+                       'cinnamon', 'black pepper'])'''
